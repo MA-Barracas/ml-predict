cd1952c57aa43f2ef3cec86f645aefe31c29f649
JLGO-UEV <joseluis.gomez.o@universidadeuropea.es>
2025-11-27 12:49:02 +0100

    bbdd

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, request
 import pandas as pd
 import pickle
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
 with open("model.pkl", "rb") as f:
     read_rf = pickle.load(f)
 
+churro = "postgresql://usuario:JFgm8AoSMDyg5QCntFL5XVCtceQ9loUG@dpg-d4fjib0dl3ps73d0nbe0-a.oregon-postgres.render.com/mypostgres_x1sa"
+engine = create_engine(churro)
 
 @app.route("/", methods=["GET"])
 def hola():
@@ -25,6 +28,15 @@ def predict():
         return "datos de miercoles"
 
     survived = "Sobrevivio" if read_rf.predict([[age, sex, clase]])[0] else "Palmo"
+
+    df = pd.DataFrame({
+    "sex":[sex],
+    "age":[age],
+    "clase": [clase],
+    "prediction": [survived]
+    })
+    df.to_sql("predictions", con=engine, if_exists="append", index=None)
+
     return f"""Para la persona con age {age}, sex {sex} y clase {clase}, 
 la prediccion es de {survived}"""
 
